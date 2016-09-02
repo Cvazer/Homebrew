3cd757f820da022267c41a7b58c60f70b7a72456
Cvazer <zhirnitoni@gmail.com>
2016-09-02 08:45:23 +0300

    Migrate to intellij

--- DnD/DnD-Info/src/main/java/by/zti/dnd/model/map/GameMap.java
+++ DnD/DnD-Info/src/main/java/by/zti/dnd/model/map/GameMap.java
@@ -1,7 +1,12 @@
 package by.zti.dnd.model.map;
 
+import java.io.Serializable;
 import java.util.LinkedList;
 
-public class GameMap {
+public class GameMap implements Serializable{
 	private LinkedList<MapCell> cells;
+
+	public GameMap() {
+		cells = new LinkedList<>();
+	}
 }
--- DnD/DnD-Info/src/main/java/by/zti/dnd/model/map/MapCell.java
+++ DnD/DnD-Info/src/main/java/by/zti/dnd/model/map/MapCell.java
@@ -2,6 +2,14 @@ package by.zti.dnd.model.map;
 
 public class MapCell {
 	private int x, y;
-	
-	
+
+	public boolean equals(Object object) {
+		if (this == object) return true;
+		if (object == null || getClass() != object.getClass()) return false;
+		if (!super.equals(object)) return false;
+
+		MapCell mapCell = (MapCell) object;
+
+        return x == mapCell.x && y == mapCell.y;
+	}
 }
