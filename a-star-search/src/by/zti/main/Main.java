ffb85d96fcfeaf48b515c012e10a18694f5c7787
Cvazer <zhirnitoni@gmail.com>
2017-05-12 13:18:26 +0300

    merge

---
+++ a-star-search/src/by/zti/main/Main.java
@@ -0,0 +1,212 @@
+package by.zti.main;
+
+import by.zti.main.model.Node;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+import static by.zti.main.model.Node.MOVE;
+import static by.zti.main.model.Node.SIZE;
+
+public class Main {
+    private static Node[][] nodes = new Node[7][7];
+
+    private static Node start;
+    private static Node end;
+
+    private static ArrayList<Node> open = new ArrayList<>();
+    private static ArrayList<Node> close = new ArrayList<>();
+
+    private static JFrame frame;
+
+    public static void main(String[] args) {
+        frame = new JFrame("A Star Test");
+        frame.setSize(717, 740);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.add(new NodeCanvas());
+        frame.setVisible(true);
+
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                calc();
+            }
+        });
+
+        frame.getComponents()[0].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                if(e.getButton()==MouseEvent.BUTTON1){
+                    setStart(nodes[e.getX()/100][e.getY()/100]);
+                    frame.repaint();
+                }
+                if(e.getButton()==MouseEvent.BUTTON3){
+                    setEnd(nodes[e.getX()/100][e.getY()/100]);
+                    frame.repaint();
+                }
+            }
+        });
+
+        create();
+        collide();
+
+        setStart(nodes[0][0]);
+        setEnd(nodes[6][6]);
+    }
+
+    private static void calc() {
+        open.clear();
+        close.clear();
+        for (Node[] row : nodes) {
+            for (Node node : row) {
+                node.setOpen(false);
+                node.setClose(false);
+                node.setPath(false);
+                node.set(0, 0);
+                node.setParent(null);
+                calcH(node);
+            }
+        }
+        Node less = start;
+        while((less != null ? less.getH() : 0) !=1){
+            less = findParents(less);
+        }
+        while(less.getX()!=start.getX() || less.getY()!=start.getY()){
+            less.setPath(true);
+            less = less.getParent();
+        }
+
+        frame.repaint();
+    }
+
+    private static void setEnd(Node node) {
+        if(end!=null){end.setEnd(false);}
+        node.setEnd(true);
+        end = node;
+        frame.repaint();
+    }
+
+    private static void setStart(Node node) {
+        if(start!=null){start.setStart(false);}
+        node.setStart(true);
+        start = node;
+        frame.repaint();
+    }
+
+    private static void collide() {
+        nodes[1][2].setCollide(true);
+        nodes[2][2].setCollide(true);
+        nodes[3][2].setCollide(true);
+        nodes[5][1].setCollide(true);
+        nodes[5][2].setCollide(true);
+        nodes[5][3].setCollide(true);
+        nodes[5][4].setCollide(true);
+        nodes[4][4].setCollide(true);
+        nodes[3][4].setCollide(true);
+        nodes[2][4].setCollide(true);
+    }
+
+    private static void create() {
+        for(int i  = 0; i < nodes.length; i++){
+            for(int k = 0; k < nodes[i].length; k++){
+                nodes[i][k] = new Node(i, k);
+            }
+        }
+    }
+
+    private static Node findParents(Node node) {
+        if(open.contains(node)){removeOpen(node);}
+        addClosed(node);
+        ArrayList<Node> toCheck = new ArrayList<>();
+        if(node.getX()-1>=0){
+            toCheck.add(nodes[node.getX()-1][node.getY()]);
+        }
+        if(node.getX()+1<=nodes.length-1){
+            toCheck.add(nodes[node.getX()+1][node.getY()]);
+        }
+        if(node.getY()-1>=0){
+            toCheck.add(nodes[node.getX()][node.getY()-1]);
+        }
+        if(node.getY()+1<=nodes[node.getX()].length-1){
+            toCheck.add(nodes[node.getX()][node.getY()+1]);
+        }
+        for(Node node_tc: toCheck){
+            if(!close.contains(node_tc) && !open.contains(node_tc) && !node_tc.isCollide()){
+                node_tc.setParent(node);
+                node_tc.set(node_tc.getH(), node.getG() + MOVE);
+                addOpen(node_tc);
+            }else if(!close.contains(node_tc) && open.contains(node_tc) && !node_tc.isCollide()){
+                if((node.getG() + MOVE) < node_tc.getG()){
+                    node_tc.setParent(node);
+                }
+            }
+        }
+        if(open.size()==0){return null;}
+        Node less = open.get(0);
+        for(Node o_node: open){
+            if(o_node.getF()<less.getF()){less = o_node;}
+        }
+        return less;
+    }
+
+    private static void removeOpen(Node node) {
+        node.setOpen(false);
+        open.remove(node);
+    }
+
+    private static void addOpen(Node node){
+        node.setOpen(true);
+        open.add(node);
+    }
+
+    private static void addClosed(Node node) {
+        node.setClose(true);
+        close.add(node);
+    }
+
+    private static void calcH(Node node) {
+        int h = 0;
+        int node_x = node.getX();
+        int node_y = node.getY();
+        while(node_x!=end.getX()){
+            h++;
+            if(node_x<end.getX()){
+                node_x++;
+            }else{
+                node_x--;
+            }
+        }
+        while(node_y!=end.getY()){
+            h++;
+            if(node_y<end.getY()){
+                node_y++;
+            }else{
+                node_y--;
+            }
+        }
+        node.set(h, 0);
+    }
+
+
+    static class NodeCanvas extends JPanel{
+        @Override
+        public void paint(Graphics g) {
+
+            for(int i  = 0; i < nodes.length; i++) {
+                for (int k = 0; k < nodes[i].length; k++) {
+                    nodes[i][k].render(g);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(i * SIZE, k * SIZE, SIZE, SIZE);
+                }
+            }
+        }
+    }
+}
---
+++ a-star-search/src/by/zti/main/model/Node.java
@@ -0,0 +1,105 @@
+package by.zti.main.model;
+
+import java.awt.*;
+
+public class Node {
+    public static final int SIZE = 100;
+    public static final int MOVE = 10;
+
+    private int x, y, g, f, h;
+    private Node parent;
+
+    private boolean open, close, collide, start, end, path;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void render(Graphics g){
+        if(open){
+            g.setColor(Color.GREEN);
+        }else if(close && !start) {
+            g.setColor(Color.RED);
+        }else if(collide) {
+            g.setColor(Color.BLACK);
+        }else if(start){
+            g.setColor(Color.BLUE);
+        }else if(end){
+            g.setColor(Color.ORANGE);
+        }else{
+            g.setColor(Color.WHITE);
+        }
+        g.fillRect(x * SIZE, y * SIZE, SIZE, SIZE);
+        g.setColor(Color.BLACK);
+        g.drawString(String.valueOf(h), (x * SIZE) + 2, (y * SIZE) + 12);
+        g.setColor(Color.CYAN);
+        if(this.g!=0){g.drawString(String.valueOf(this.g), (x * SIZE) + 2, (y * SIZE) + 22);}
+        g.setColor(Color.GRAY);
+        if(this.g!=0){g.drawString(String.valueOf(f), (x * SIZE) - 5 + SIZE/2, (y * SIZE) + 8 + SIZE/2);}
+        g.setColor(Color.GRAY);
+        if(path){g.fillOval((x * SIZE) + SIZE/2 - SIZE/8, (y * SIZE) + SIZE/2 - SIZE/8, SIZE/4, SIZE/4);}
+    }
+
+    public void set(int h, int g){
+        this.h = h;
+        this.g = g;
+        f = h + g;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public void setOpen(boolean open) {
+        this.open = open;
+    }
+
+    public void setClose(boolean close) {
+        this.close = close;
+    }
+
+    public boolean isCollide() {
+        return collide;
+    }
+
+    public void setCollide(boolean collide) {
+        this.collide = collide;
+    }
+
+    public void setStart(boolean start) {
+        this.start = start;
+    }
+
+    public void setEnd(boolean end) {
+        this.end = end;
+    }
+
+    public void setPath(boolean path) {
+        this.path = path;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public int getG() {
+        return g;
+    }
+
+    public int getF() {
+        return f;
+    }
+}
